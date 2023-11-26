276c69ab19716a48c6e2afea771c7135c26925d1
99499 <99499@DESKTOP-NUA3EGJ>
2023-11-26 19:31:37 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Vvedite stroku;");
+string str = Console.ReadLine();
+int count = 0;
+
+string[] kuki = str.Split(' ');
+string str1 = kuki[kuki.Length - 1];
+
+for(int i = 0; i < str1.Length; i++)
+{
+    if (str1[i] == 'a')
+    {
+        count++;
+    }
+}
+
+Console.WriteLine(count);
